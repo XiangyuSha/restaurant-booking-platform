235a57262afa5270b8f3b60a11d5792b04247ea0
Xiangyu Sha <109414968+XiangyuSha@users.noreply.github.com>
2025-03-15 21:05:51 -0700

    Force add views directory

---
+++ Client/views/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div >
+      Hello world!
+    </div>
+  );
+}
+
+export default App;
+
+// https://restaurant-booking-platform-534361869622.us-east1.run.app
---
+++ Client/views/src/config.js
@@ -0,0 +1,5 @@
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+console.log("API Base URL:", API_BASE_URL);
+
+export default API_BASE_URL;
